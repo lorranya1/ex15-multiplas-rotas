3ae5e860b946033d95466224f29b421883c029b1
lorranya1 <lar2@aluno.ifal.edu.br>
2026-06-12 15:11:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 // TODO: Implemente as rotas:
-// GET /       → 200, corpo contém "Home"
-// GET /sobre  → 200, corpo contém "Sobre"
-// GET /contato→ 200, corpo contém "Contato"
+GET /       → 200, corpo contém "Home"
+GET /sobre  → 200, corpo contém "Sobre"
+GET /contato→ 200, corpo contém "Contato"
 // Rotas não mapeadas → 404 automático do Express
 
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));

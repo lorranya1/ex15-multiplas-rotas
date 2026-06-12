c9d0536d339a266ce8e8226a96f09d9c38718b9d
lorranya1 <lar2@aluno.ifal.edu.br>
2026-06-12 15:25:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 
 // TODO: Implemente as rotas:
-GET /       → 200, corpo contém "Home"
-GET /sobre  → 200, corpo contém "Sobre"
-GET /contato→ 200, corpo contém "Contato"
-GET /rota-inexistente → 404
-// Rotas não mapeadas → 404 automático do Express
+//GET /       → 200, corpo contém "Home"
+app.get('/', (req, res) => res.send('Home'));
+//GET /sobre  → 200, corpo contém "Sobre"
+app.get('/sobre', (req, res) => res.send('Sobre'));
+//GET /contato→ 200, corpo contém "Contato"
+app.get('/contato', (req, res) => res.send('Contato'));
+//GET /rota-inexistente → 404
+app.use((req, res) => res.status(404).send('Rota não encontrada'));
 
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
